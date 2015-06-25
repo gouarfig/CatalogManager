fe8b70bf56868e18a46879a2ebafde9c82439205
Fred Quointeau <fred.quointeau@live.com>
2015-06-25 09:18:10 +0100

    Added missing files

---
+++ BusinessObjects.Tests/GenericTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace BusinessObjects.Tests
+{
+	[TestFixture]
+    public class GenericTests
+    {
+		private void DoSomethingInALoop(int i)
+		{
+		}
+
+		[Test]
+		public void NormalLoop()
+		{
+			for (int i = 0; i < 1000; i++)
+			{
+				DoSomethingInALoop(i);
+			}
+		}
+
+		[Test]
+		public void ImprovedLoop()
+		{
+			Parallel.For(0, 1000, i => DoSomethingInALoop(i));
+		}
+
+		//TODO do some ReadAsync and await
+    }
+}
