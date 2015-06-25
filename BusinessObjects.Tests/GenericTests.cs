c9398d6d2d901ae3435a1e973e7a5a405b1a960d
Fred Quointeau <fred.quointeau@live.com>
2015-06-25 09:35:00 +0100

    Added new tests to TravisCI

--- BusinessObjects.Tests/GenericTests.cs
+++ BusinessObjects.Tests/GenericTests.cs
@@ -1,32 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using NUnit.Framework;
 
 namespace BusinessObjects.Tests
 {
 	[TestFixture]
-    public class GenericTests
-    {
+	[Category("travisci")]
+	public class GenericTests
+	{
+		private const int LoopCount = 60;
+
 		private void DoSomethingInALoop(int i)
 		{
+			Thread.Sleep(1000);
 		}
 
 		[Test]
 		public void NormalLoop()
 		{
-			for (int i = 0; i < 1000; i++)
+			Stopwatch stopwatch = Stopwatch.StartNew();
+			for (int i = 0; i < LoopCount; i++)
 			{
 				DoSomethingInALoop(i);
 			}
+			stopwatch.Stop();
+			Debug.Print("NormalLoop: Time elapsed = {0}ms", stopwatch.ElapsedMilliseconds);
+			Assert.That(stopwatch.ElapsedMilliseconds, Is.InRange(50000, 70000));
 		}
 
 		[Test]
 		public void ImprovedLoop()
 		{
-			Parallel.For(0, 1000, i => DoSomethingInALoop(i));
+			Stopwatch stopwatch = Stopwatch.StartNew();
+			Parallel.For(0, LoopCount, i => DoSomethingInALoop(i));
+			stopwatch.Stop();
+			Debug.Print("ImprovedLoop: Time elapsed = {0}ms", stopwatch.ElapsedMilliseconds);
+			Assert.That(stopwatch.ElapsedMilliseconds, Is.InRange(10000, 40000));
 		}
 
 		//TODO do some ReadAsync and await
